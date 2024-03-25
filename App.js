6eea4ef853070dbbb0523f07c28f9edae12e0c77
PaulEtignard <Paul.etignard@gmail.com>
2024-03-25 10:04:57 +0100

    les proprs

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text,View } from "react-native";
+import { Text,View, Image } from "react-native";
 import { User } from "./components/User";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import {s} from "./App.style"
@@ -8,7 +8,21 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{backgroundColor:"red", flex:1}}>
-        <FlexDemo/>
+        <User
+          firstName={"Paul"}
+          lastName={"Doe"}
+          age={40}
+          adress={{street: "8B rue jean wyrch", city:"Besançon"}}
+          isVerified={true}
+          doSomething={function (){
+            console.log("User authenticated")
+          }}
+        >
+          <Image
+              style={{height: 200, with:300}}
+              source={{uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
+          />
+        </User>
       </SafeAreaView>
     </SafeAreaProvider>
     
--- components/User.js
+++ components/User.js
@@ -1,5 +1,17 @@
-import { Text } from "react-native";
+import { Text,Image } from "react-native";
 
-export function User(){
-    return <Text>Je suis un user</Text>
+export function User({firstName, lastName, age, adress, isVerified, doSomething, children}) {
+    doSomething()
+    return <>
+        
+        {children}
+        <Text>
+            Mon nom est {firstName} {lastName}, j'ai {age} ans.
+            Mon age est {age} ans. {"\n"}
+            {isVerified ? "Mon compte est vérifié" : "Mon compte n'est pas vérifié"} {"\n"}
+        </Text>
+        <Text>
+            Mon adresse est {adress.street}, {adress.city}
+        </Text>
+    </>
 }
