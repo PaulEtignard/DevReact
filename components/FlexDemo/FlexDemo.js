610b49e6d9e97acb7504fd423a730dc15e4da65f
PaulEtignard <Paul.etignard@gmail.com>
2024-03-25 09:39:47 +0100

    Flex

--- components/FlexDemo/FlexDemo.js
+++ components/FlexDemo/FlexDemo.js
@@ -4,6 +4,9 @@ import {View} from "react-native"
 export function FlexDemo(){
     return(
         <View style={s.container}>
+            <View style={s.box1}/>
+            <View style={s.box2}/>
+            <View style={s.box3}/>
         </View>
     )
 }
--- components/FlexDemo/FlexDemo.style.js
+++ components/FlexDemo/FlexDemo.style.js
@@ -3,6 +3,24 @@ import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
     container : {
         flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
         backgroundColor: "black"
+    },
+    box1: {
+        height: 100,
+        width: 100,
+        backgroundColor: "blue"
+    },
+    box2: {
+        height: 100,
+        width: 100,
+        backgroundColor: "white"
+    },
+    box3: {
+        height: 100,
+        width: 100,
+        backgroundColor: "grey"
     }
 })
